d23390dff956b533fe404418dd7b27471d22f9fa
Youjin Park <beecomci@gmail.com>
2021-11-21 01:45:02 +0900

    data 바인딩, 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="menu">
+      <a href="#" v-for="(item, i) in menus" :key="i">{{ item }}</a>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-for="(item, i) in products" :key="i">
+      <h4 :style="h4Style">{{ item }}</h4>
+      <p>50 만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    // 데이터 보관함
+    return {
+      h4Style: "color: blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menus: ["Home", "Shop", "About"]
+    };
+  },
+  components: {}
+};
 </script>
 
 <style>
@@ -21,6 +33,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
